8f2f0b2d3524b30261b09f57e8fcbdfffa1a04c8
Emi Simpson <emi@alchemi.dev>
2019-12-28 18:08:54 -0500

    Moved some logic from main to a dedicated function that emulates commandline functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
 pub mod asset;
 pub mod resources;
 pub mod util;
+
+use reqwest::Client;
+use reqwest::Url;
+
+use asset::Asset;
+
+pub async fn download_complete_page(url: Url) -> asset::Result<String> {
+    let client = Client::new();
+    let mut asset = Asset::new(
+        url,
+        "text/plain".to_owned(),
+    );
+    asset.download_complete(&client).await?;
+    asset.try_stringify()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use reqwest::Client;
 use reqwest::Url;
 
-use monolith::asset::Asset;
+use monolith;
 
 #[tokio::main(single_thread)]
 async fn main() {
-    let client = Client::new();
-    let mut asset = Asset::new(
-        Url::parse("http://localhost:2015/").unwrap(),
-        "text/plain".to_string(),
-    );
-    asset.download_complete(&client).await.unwrap();
     println!(
         "{}",
-        asset.try_stringify().unwrap()
+        monolith::download_complete_page(
+            Url::parse(
+                &std::env::args()
+                    .skip(1)
+                    .next()
+                    .expect("Missing argument")
+            ).expect("Bad URL")
+        )
+            .await
+            .expect("Error getting page")
     );
 }
